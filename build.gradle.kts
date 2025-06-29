37616659aacd7ebff8d55e78b46060422de0c658
Rbeat542 <a.makevnin@gmail.com>
2025-06-29 22:47:11 +0300

    build.gradle.kts update

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.7"
 	id("org.sonarqube") version "6.2.0.5505"
 	id("io.freefair.lombok") version "8.13.1"
-	id("io.sentry.jvm.gradle") version "5.8.0"
 	jacoco
 }
 
