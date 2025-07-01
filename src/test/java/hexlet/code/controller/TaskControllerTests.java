f6fb351781c2db6307527edbb4380537aa44b337
Rbeat542 <a.makevnin@gmail.com>
2025-07-01 11:31:11 +0300

    checkstyle error fix

--- src/test/java/hexlet/code/controller/TaskControllerTests.java
+++ src/test/java/hexlet/code/controller/TaskControllerTests.java
@@ -24,8 +24,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import java.nio.charset.StandardCharsets;
-import java.util.Set;
-
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
